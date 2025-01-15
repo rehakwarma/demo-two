0e48e9bf18c1d204652e274c6a87b80a0f6f619d
Mahendra Rehakwar <rehakwarmahendra@gmail.com>
2025-01-16 00:03:40 +0530

    i added new file to the master

---
+++ piso.v
@@ -0,0 +1,19 @@
+Verilog Code
+The verilog code for PISO shift register is shown below.
+
+module Shiftregister_PISO(Clk, Parallel_In,load, Serial_Out);
+input Clk,load;
+input [3:0]Parallel_In;
+output reg Serial_Out;
+reg [3:0]tmp;
+always @(posedge Clk)
+begin
+if(load)
+tmp<=Parallel_In;
+else
+begin
+Serial_Out<=tmp[3];
+tmp<={tmp[2:0],1’b0};
+end
+end
+endmodule

22d4bf44a58cf54bc05ffc8225ed552c34205ff9
Mahendra Rehakwar <rehakwarmahendra@gmail.com>
2025-01-15 23:27:58 +0530

    some changes did in html file

--- html.v
+++ html.v
@@ -1,2 +1,42 @@
- i am a boy and i live at murdara and naren is my friend
- Piyush is like my brother but nareen will give me chance to get place in a good company.i
+     This code for creating the html form
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Employee Details</title>
+</head>
+<body>
+  <form>
+    <fieldset>
+    <legend>Employee Details</legend>
+     <p>
+         First name: <input type = "text" name = "fname" />
+  </p>
+   <p>
+         Last name: <input type = "text" name = "lname" />
+  </p>
+ 
+   <p>
+         <input type = "radio" name = "Gender" value = "Male"> Male
+         <input type = "radio" name = "Gender" value = "Female"> Female
+</p>
+ 
+<p>
+Employee ID: <input type = "text" name = "ID" />
+</p>
+ 
+<p>
+Designation: <input type = "text" name = "ID" />
+</p>
+ 
+<p>
+Phone Number: <input type = "text" name = "phone" />
+</p>
+ 
+ <p>
+      <input type = "submit" name = "submit" value = "Submit" />
+    </p>
+</fieldset>
+</form>
+</body>
+</html>

c39fff6ea2cd465bd45e1f8e6c08b63fe0aed7ad
Mahendra Rehakwar <rehakwarmahendra@gmail.com>
2025-01-15 23:22:44 +0530

    new file is added

---
+++ sipo.v
@@ -0,0 +1,29 @@
+module sipomod(clk,clear, si, po);
+
+input clk, si,clear;
+
+output [3:0] po;
+
+reg [3:0] tmp;
+
+reg [3:0] po;
+
+always @(posedge clk)
+
+begin
+
+if (clear)
+
+tmp <= 4’b0000;
+
+else
+
+tmp <= tmp << 1;
+
+tmp[0] <= si;
+
+po = tmp;
+
+end
+
+endmodule
